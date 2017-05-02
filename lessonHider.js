b9c8081942a9b87071768daa8a995d9c1fae4004
loren pabst <loren pabst>
2017-05-02 13:46:18 -0600

    everything through step 4 done

--- lessonHider.js
+++ lessonHider.js
@@ -2,7 +2,14 @@ angular.module('directivePractice')
 .directive('lessonHider', function(){
 
     return {
-
+        restrict: 'E',
+        templateUrl: 'lessonHider.html',
+        scope: {
+            lesson: '=',
+        },
+        link: function (scope, elem, atts){
+            
+        }
     }
 
 
